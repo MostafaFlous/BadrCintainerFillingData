1eb525dee7846fa252660053375030a9ddcee98d
MostafaFlous <mostafa.flous@gmail.com>
2023-03-05 07:13:04 +0200

    cashent

---
+++ Object Repository/NewCashRentAgelRepo/Page_Badr Container/select__1.rs
@@ -0,0 +1,157 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select__1</name>
+   <tag></tag>
+   <elementGuidId>f135b1b8-ba44-43fc-a136-5c56fee18a68</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(//select[@name='classification_id'])[2]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>select</value>
+      <webElementGuid>637d819e-8710-4c8e-bd1a-9d7f8f6870ed</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>form-control classification_id</value>
+      <webElementGuid>c692c442-1d5d-49f0-8e43-01c01b5a0330</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>classification_id</value>
+      <webElementGuid>3fc2d9f0-43eb-4667-b7ed-04f92ceafa6f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>oninvalid</name>
+      <type>Main</type>
+      <value>this.setCustomValidity('please select classification')</value>
+      <webElementGuid>af2f043e-2744-4a85-9cbc-87400418af44</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>oninput</name>
+      <type>Main</type>
+      <value>setCustomValidity('')</value>
+      <webElementGuid>d12c5cc1-a2be-43c5-af51-78f19f3f0c91</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                        برجاء اختر التصنيف
+
+
+                                                    نفايات
+                                                    انقاض
+                                            </value>
+      <webElementGuid>aecb870c-d71a-423c-a8de-b3cbacbae153</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;form-cash&quot;)/div[@class=&quot;col-md-12 form-group float-e-margins&quot;]/div[@class=&quot;col-md-4&quot;]/select[@class=&quot;form-control classification_id&quot;]</value>
+      <webElementGuid>fda98d71-4988-4d86-b830-c5108ea225e0</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>(//select[@name='classification_id'])[2]</value>
+      <webElementGuid>c1d839d8-900f-4698-9dee-74805d9fba95</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='form-cash']/div[2]/div/select</value>
+      <webElementGuid>ad5eae9a-004e-4819-be82-53ca5d3b8f88</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='التصنيف'])[2]/following::select[1]</value>
+      <webElementGuid>87f27521-e600-4fec-b4eb-1215682cd8ed</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='الى'])[2]/following::select[1]</value>
+      <webElementGuid>439a11f5-2821-4aee-80a4-01ce0e0b04a1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='حجم الحاوية'])[2]/preceding::select[1]</value>
+      <webElementGuid>9ae76be7-f5e2-46a7-bb4d-8b5d4cd29ec9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='رقم الحاوية'])[2]/preceding::select[2]</value>
+      <webElementGuid>eafe6fa9-c849-44f2-8ef2-79095f0e73b0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//form[2]/div[2]/div/select</value>
+      <webElementGuid>4e177bc5-4f77-418a-b4ab-4e79ab497b9e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//select[@name = 'classification_id' and (text() = '
+                        برجاء اختر التصنيف
+
+
+                                                    نفايات
+                                                    انقاض
+                                            ' or . = '
+                        برجاء اختر التصنيف
+
+
+                                                    نفايات
+                                                    انقاض
+                                            ')]</value>
+      <webElementGuid>9ddd101a-4fa7-4962-8a2e-4f1ad1a28522</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
